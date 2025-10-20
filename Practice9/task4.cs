8db6117dfce1dac3844cfebb87d2e9c31d07bda3
DagestanBratka <aegor7540@gmail.com>
2025-10-21 00:27:58 +0300

    Add files via upload

---
+++ Practice9/task4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp42
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(0);
+            Console.Write("Введите радиус: ");
+            if (!double.TryParse(Console.ReadLine(), out double num) || num <= 0)
+            {
+                Console.WriteLine("Ошибка. Радиус должен быть больше 0");
+                return;
+            }
+            circle.RADIUS = num;
+            circle.GetArea();
+        }
+    }
+    class Circle
+    {
+        private double radius;
+        private double S;
+        public Circle(double Radius)
+        {
+            radius = Radius;
+        }
+        public void GetArea()
+        {
+            S = radius * radius * Math.PI;
+            Console.WriteLine($"Площадь круга: {S}");
+        }
+        public double RADIUS
+        {
+            set { radius = value; }
+            get { return radius; }
+        }
+    }
+}
